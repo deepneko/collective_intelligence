7cd7cf36dabd4fed49694995fcc5ef1a801a357b
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2008-11-21 22:10:10 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/collective_intelligence@73 76beb010-293a-44e9-8231-3d3008485057

--- chapter4/searcher.rb
+++ chapter4/searcher.rb
@@ -22,7 +22,11 @@ class Searcher
     wordids = []
     
     #separate words with space
-    words = q.split(/\s/)
+    if q
+      words = q.split(/\s/)
+    else
+      return
+    end
     tablenumber = 0
 
     words.each { |word|
@@ -47,8 +51,75 @@ class Searcher
 
     return cur
   end
+
+  def getscoredlist(rows, wordids)
+    totalscores = Hash.new
+    rows.each { |row|
+      totalscores[row[0]] = 0.0
+    }
+    
+    #TODO すべて1.0の重みづけがしてあるが・・・？
+    weights = frequencyscore(rows)
+
+    totalscores.each_key { |url|
+      totalscores[url] += weights[url]
+    }
+
+    return totalscores
+  end
+
+  def geturlname(id)
+    cur = @con.execute("select url from urllist where rowid='#{id}'")
+    return cur[0]
+  end
+
+  def query(q)
+    rows = wordids = getmatchrows(q)
+    scores = getscoredlist(rows, wordids)
+    scores.to_a.sort{|a, b|
+      (b[1] <=> a[1]) * 2 + (a[0] <=> b[0])
+    }
+    
+    scores.each { |s|
+      print s[1].to_s + " "
+      p geturlname(s[0])
+    }
+  end
+
+  def normalizescores(scores, smallIsBetter=0)
+    vsmall = 0.00001
+    nscores = Hash.new
+    if smallIsBetter == 1
+      minscore = scores.values.min
+      scores.each_pair { |u, l|
+        t = [vsmall, l]
+        nscores[u] = minscore.to_f / t.max
+      }
+    else
+      maxscore = scores.values.max
+      if maxscore == 0
+        maxscore = vsmall
+      end
+      scores.each_pair { |u, c|
+        nscores[u] = c.to_f / maxscore
+      }
+    end
+    return nscores
+  end
+
+  def frequencyscore(rows)
+    counts = Hash.new
+    rows.each { |row|
+      counts[row[0]] = 0
+    }
+    rows.each { |row|
+      counts[row[0]] += 1
+    }
+    return normalizescores(counts)
+  end
 end
 
 const = Const.new
 searcher = Searcher.new(const.dbname)
-p searcher.getmatchrows(ARGV[0])
+#p searcher.getmatchrows(ARGV[0])
+searcher.query(ARGV[0])
