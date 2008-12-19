ad9428d1dc90e3d9757557eca746a656b67d9b23
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2008-12-19 23:05:10 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/collective_intelligence@101 76beb010-293a-44e9-8231-3d3008485057

--- chapter5/twittergraph.rb
+++ chapter5/twittergraph.rb
@@ -2,6 +2,38 @@ require 'rubygems'
 gem 'twitter4r', '>=0.3.0'
 require 'twitter'
 require 'twitter/console'
+require 'sqlite3'
+require 'curl'
+
+class TwitterGraph
+  def initialize(friends)
+    @friendlist = friends
+    @con = SQLite3::Database.new("twitter.db")
+  end
+
+  def createdb
+    first = @friendlist.shift.screen_name
+    sql = "create table friendsmatrix (" + first;
+    @friendlist.each do |f|
+      sql += "," + f.screen_name
+    end
+    sql += ")"
+    @friendlist.unshift(first)
+    @con.execute(sql)
+  end
+
+  def initdb
+    @friendlist.each do |f|
+      
+    end
+  end
+
+  def isfriend(user_a, user_b)
+#    curl = Curl::Easy.perform("http://twitter.com/friendships/exsists.json?user_a=#{user_a}&user_b=#{user_b}")
+    curl = Curl::Easy.perform("http://twitter.com/")
+    p curl.body_str
+  end
+end
 
 twitter = Twitter::Client.new(
                               "login" => "deepneko",
@@ -9,14 +41,12 @@ twitter = Twitter::Client.new(
                               )
 
 friends = twitter.my(:friends)
-p friends.size
+#p friends.size
 
-friends.each do |friend|
-  p friend.screen_name
-end
+#friends.each do |friend|
+#  p friend.screen_name
+#end
 
-
-
-
-
-
+twittergraph = TwitterGraph.new(friends)
+#twittergraph.createdb
+twittergraph.isfriend("ryo_katsuma", "gakkie")
